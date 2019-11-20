7e038a3eecd9c439b6a9ecd2c8249b0181f25e8f
Luiza Zavalan <38529623+LuizaZavalan@users.noreply.github.com>
2019-11-20 16:12:27 +0000

    Add files via upload

---
+++ sivellsf90/mod_contr.f
@@ -0,0 +1,10 @@
+      module contr
+      use kinddefine
+      implicit none
+      character(len=4,kind=K3),dimension(3) :: itle
+      character(len=4,kind=K3) :: mc
+      integer(kind=K4),save :: ie,lr,it,jb,jq,jx,kat,kbl,king,ko,lv
+      integer(kind=K4),save :: nocon,in,mcp,ip,iq,ise,jc,m,mp,mq,n,np
+      integer(kind=K4),save :: nf,nut,nr,lc,md,mf,mt,nd,nt
+      end module contr
+
